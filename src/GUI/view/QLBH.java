ee3e07cf7c3df98b17166a89bc4a7ffa618217fa
Nhan <thanhnhan140705@gmail.com>
2025-04-29 20:51:47 +0700

    uppppp

--- src/GUI/view/QLBH.java
+++ src/GUI/view/QLBH.java
@@ -10,9 +10,11 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -57,6 +59,7 @@ public class QLBH extends JPanel{
         container.add(HD(), BorderLayout.NORTH);
         
         JPanel bottom= new JPanel();
+        bottom.setLayout(new BorderLayout());
         Border bd= BorderFactory.createLineBorder(Color.BLACK);
         TitledBorder ttBd= new TitledBorder("Thông tin hóa đơn");
         ttBd.setTitleColor(Color.GREEN);
@@ -64,6 +67,9 @@ public class QLBH extends JPanel{
         ttBd.setTitleFont(new Font("Arial", Font.PLAIN, 22));
         bd= BorderFactory.createTitledBorder(ttBd);
         bottom.setBorder(bd);
+        
+        bottom.add(left(), BorderLayout.WEST);
+        bottom.add(right());
         
         container.add(bottom, BorderLayout.CENTER);
         add(container, BorderLayout.CENTER);
@@ -128,15 +134,72 @@ public class QLBH extends JPanel{
     
     public JPanel left(){
         JPanel p= new JPanel();
-        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        JPanel out= new JPanel();
+        out.setLayout(new BoxLayout(out, BoxLayout.Y_AXIS));
         JPanel img= new JPanel();
         img.setSize(300, 200);
         img.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        JPanel[] input= new JPanel[4];
-        for(int i=0; i< 4; i++){
+        JPanel[] input= new JPanel[5];
+        out.add(img);
+        ImageIcon icon= new ImageIcon(getClass().getResource("../../img/search.png"));
+        Image img1= icon.getImage();
+        img1= img1.getScaledInstance(300, 200, Image.SCALE_AREA_AVERAGING);
+        JLabel label = new JLabel(new ImageIcon(img1));
+        img.add(label);
+        for(int i=0; i< 5; i++){
             input[i]= new JPanel();
             input[i].setLayout(new GridLayout(1, 2));
+            out.add(input[i]);
         }
+        JPanel temp= new JPanel();
+        temp.add(maSach);
+        temp.add(ctMaSach);
+        input[0].add(new JLabel("Mã sách"));
+        input[0].add(temp);
+        input[1].add(new JLabel("Tên sách"));
+        input[1].add(tenSach);
+        input[2].add(new JLabel("Số lượng"));
+        input[2].add(soLg);
+        input[3].add(new JLabel("Đơn giá"));
+        input[3].add(donGia);
+        input[4].add(new JLabel("Mã GG"));
+        input[4].add(maGG);
+        out.add(them);
+        
+        p.add(out);
+        return p;
+    }
+    
+    public JPanel right(){
+        JPanel p= new JPanel();
+        p.setLayout(new BorderLayout());
+        
+        DefaultTableModel model= (DefaultTableModel) table.getModel();
+        String[] column= new String[]{"Mã sách", "Tên sách", "Số lượng", "Đơn giá", "Giảm giá", "Thành tiền"};
+        model.setColumnIdentifiers(column);
+        model.setRowCount(20);
+        JScrollPane pane= new JScrollPane(table);
+        p.add(pane);
+        
+        JPanel bottom= new JPanel();
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.Y_AXIS));
+        JPanel up= new JPanel();
+        up.setLayout(new GridLayout(1, 6));
+        up.add(new JLabel("Tổng tiền"));
+        up.add(tongTien);
+        up.add(new JLabel("Giảm giá"));
+        up.add(giamGia);
+        up.add(new JLabel("Thành tiền"));
+        up.add(thanhTien);
+        bottom.add(up);
+        JPanel down= new JPanel();
+        down.setLayout(new GridLayout(1, 4));
+        down.add(xacNhan);
+        down.add(huy);
+        down.add(sua);
+        down.add(xoa);
+        bottom.add(down);
+        p.add(bottom, BorderLayout.SOUTH);
         
         return p;
     }
